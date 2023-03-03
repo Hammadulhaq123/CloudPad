98021c61a946941263b0fed77455cdd45bab5f96
Hammadulhaq123 <professorcoding123@gmail.com>
2023-03-04 00:09:43 +0500

    Created a thunderclient collection to check database connectivity and customized the code by using comments. Also omitted promise.then.catch and used async await to use try and catch block for error handling

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,29 +4,56 @@ const User = require('../models/User')
 // ...rest of the initial code omitted for simplicity.
 const { body, validationResult } = require('express-validator');
 
+
+
+// Create a user  using POST method: "/api/auth/createuser/" "No Login required"
 router.post(
-    '/',
+    '/createuser',
     // name must be atleast 3 char
     body('name', "Enter a valid name").isLength({ min: 3 }),
     // email must be an email
     body('email', "Enter a valid email").isEmail(),
     // password must be at least 5 chars long
     body('password', "Password must be more than 5 characters").isLength({ min: 5 }),
-    (req, res) => {
+    async (req, res) => {
+
+
+
         // Finds the validation errors in this request and wraps them in an object with handy functions
+        // If there are errors return Bad request and errors
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         }
 
-        User.create({
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password,
-        }).then(user => res.json(user)).catch(err => {
-            console.log(err)
-            res.json({ error: "Email already in use" })
-        })
+        try {
+            // Checking wheather the user with same email already exist :
+
+            let user = await User.findOne({ email: req.body.email })
+
+            if (user) {
+                return res.status(400).json({ error: 'User with this email already exists' })
+            }
+            user = await User.create({
+                name: req.body.name,
+                email: req.body.email,
+                password: req.body.password,
+            })
+
+
+            // .then(user => res.json(user)).catch(err => {
+            //     console.log(err)
+            //     res.json({ error: "Email already in use" })
+            // })
+
+            res.json(user)
+
+        } catch (error) {
+            console.error(error.message)
+            res.status(500).send("Some Error Occured")
+        }
+
+
     },
 );
 
