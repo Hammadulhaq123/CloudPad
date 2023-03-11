8b0a612b0e2ca75a3d31cc6953d41d517c78493c
Hammadulhaq123 <professorcoding123@gmail.com>
2023-03-11 18:42:14 +0500

    Created more routes to authenticate user by their Id and getting the id through middleware

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,26 +1,34 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/User')
+const User = require('../models/User');
+// Express Validator to apply checks on fields
 const { body, validationResult } = require('express-validator');
 // Adding bcrypt to use hashing;
 const bcrypt = require('bcrypt')
 // Adding JWT Authentication:
 const jwt = require('jsonwebtoken')
+// Fetching the middleware function
+const fetchdata = require('../middleware/fetchdata')
 
 
+
+
+// JWT SECRET should be stored in an env but its not a commercial app so we are posting it here
+const JWT_SECRET = 'Iamasecret1!anddb#$depends&^onme'
+
+// ROUTE 1:
 
 // Create a user  using POST method: "/api/auth/createuser/" "No Login required"
 router.post(
-    '/createuser',
+    '/createuser', [
     // name must be atleast 3 char
     body('name', "Enter a valid name").isLength({ min: 3 }),
     // email must be an email
     body('email', "Enter a valid email").isEmail(),
     // password must be at least 5 chars long
-    body('password', "Password must be more than 5 characters").isLength({ min: 5 }),
+    body('password', "Password must be more than 5 characters").isLength({ min: 5 })],
     async (req, res) => {
 
-        const JWT_SECRET = 'Iamasecret1!anddb#$depends&^onme'
 
         // Finds the validation errors in this request and wraps them in an object with handy functions
         // If there are errors return Bad request and errors
@@ -76,5 +84,71 @@ router.post(
 
     },
 );
+
+
+// ROUTE 2:
+
+// Authenticate a user  using POST method: "/api/auth/login/" "No Login required"
+router.post(
+    '/login', [
+    // email must be an email
+    body('email', "Enter a valid email").isEmail(),
+    // password cannot be blank
+    body('password', "Password cannot be blank").exists()],
+    async (req, res) => {
+        // Finds the Authentication errors in this request and wraps them in an object with handy functions
+        // If there are errors return Bad request and errors
+
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        const { email, password } = req.body;
+        try {
+            // Finding the email in our db
+            let user = await User.findOne({ email })
+            // If invalid email
+            if (!user) {
+                return res.status(400).json({ error: "Enter Valid Credentials" })
+            }
+            // Comparing the Password in the db and the one that user entered
+            const passwordCompare = await bcrypt.compare(password, user.password);
+            // If invalid password
+            if (!passwordCompare) {
+                return res.status(400).json({ error: "Enter Valid Credentials" })
+            }
+            const data = {
+                user: {
+                    id: user.id,
+                }
+            }
+            const authtoken = jwt.sign(data, JWT_SECRET)
+
+
+            // Sending  auth token in response
+            res.json({ authtoken })
+
+        } catch (error) {
+            console.error(error.message)
+            res.status(500).send("Internal Server Error")
+        }
+    },
+);
+
+
+// ROUTE 3:
+
+// Get loggedin User Details using POST method: "/api/auth/getdetail/" "Login required"
+router.post(
+    '/getdetail', fetchdata, async (req, res) => {
+        try {
+            userId = req.user.id
+            const user = await User.findById(userId).select("-password")
+            res.send(user)
+        } catch (error) {
+            console.error(error.message)
+            res.status(500).send("Internal Server Error")
+        }
+    })
 
 module.exports = router;
