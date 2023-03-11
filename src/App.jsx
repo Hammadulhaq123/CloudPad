429e7740e7cd9e3d1ee20866688ed51797180095
Hammadulhaq123 <professorcoding123@gmail.com>
2023-03-12 01:23:36 +0500

    Initialized react-router-dom and used it inside our application

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+
+
+// import './App.css'
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import About from "./components/About"
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <h1>This is Inotebook</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path='/' element={
+            <>
+              <Home />
+            </>
+          } />
+          <Route path='/about' element={
+            <>
+              <About />
+            </>
+          } />
+        </Routes>
+
+      </Router>
+    </>
   )
 }
 
