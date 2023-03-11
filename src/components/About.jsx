189da958b3d3b10250f609425d639440d1ae0363
Hammadulhaq123 <professorcoding123@gmail.com>
2023-03-12 01:22:27 +0500

    Created dummy Components for to use them later

---
+++ src/components/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Box, Typography } from '@mui/material'
+const About = () => {
+    return (
+        <Box sx={{ position: 'absolute', top: '11.5vh' }}>
+            <Typography variant='h6'>This is About</Typography>
+        </Box>
+    )
+}
+
+export default About
---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Box, Typography } from '@mui/material'
+const Home = () => {
+    return (
+        <Box sx={{ position: 'absolute', top: '11.5vh' }}>
+            <Typography variant='h6' >This is Home</Typography>
+        </Box>
+    )
+}
+
+export default Home
