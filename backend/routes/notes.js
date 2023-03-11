ec58496187c3111e8435fc210c4bbb9033769e45
Hammadulhaq123 <professorcoding123@gmail.com>
2023-03-11 18:43:02 +0500

    Created endpoints for fetching all notes of a user and adding notes of a user to the backend

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,11 +1,61 @@
 const express = require('express');
 const router = express.Router();
+// Fetching the middleware function
+const fetchdata = require('../middleware/fetchdata')
+// importing notes model
+const Notes = require('../models/Notes')
+// Express Validator to apply checks on fields
+const { body, validationResult } = require('express-validator');
 
 
-router.get('/', (req, res) => {
 
-    res.json([]);
+// ROUTE 1:
+
+// Get All notes data using GET method: "/api/auth/getdetail/" "Login required"
+router.get('/fetchallnotes', fetchdata, async (req, res) => {
+    try {
+        const notes = await Notes.find({ user: req.user.id })
+        res.json(notes);
+
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).send("Internal Server Error")
+    }
 })
+
+
+// ROUTE 2:
+
+// Add Notes using POSt method: "/api/auth/getdetail/" "Login required"
+router.post('/addnotes', fetchdata, [
+    // Title must be atleast 3 char
+    body('title', "Must Enter a Title").exists(),
+    // description must be at least 3 chars long
+    body('description', "Description must be of 3 Words").isLength({ min: 3 })],
+    async (req, res) => {
+        // To handle error using try catch
+        try {
+            // Using destructuring
+            const { title, description, tag } = req.body
+
+            // If there are errors return Bad request and errors
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
+            }
+
+
+
+            const note = new Notes({
+                title, description, tag, user: req.user.id
+            })
+            const savedNote = await note.save()
+            res.json(savedNote);
+        } catch (error) {
+            console.error(error.message)
+            res.status(500).send("Internal Server Error")
+        }
+    })
 
 
 module.exports = router;
